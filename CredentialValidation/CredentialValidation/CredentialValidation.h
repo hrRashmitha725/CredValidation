beb5e3928b572ced8e577c038907627152e5299d
hrRashmitha257 <rashmithahr777@gmail.com>
2022-12-01 15:28:10 +0530

    added framework

---
+++ CredentialValidation/CredentialValidation/CredentialValidation.h
@@ -0,0 +1,18 @@
+//
+//  CredentialValidation.h
+//  CredentialValidation
+//
+//  Created by Softsuave on 01/12/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CredentialValidation.
+FOUNDATION_EXPORT double CredentialValidationVersionNumber;
+
+//! Project version string for CredentialValidation.
+FOUNDATION_EXPORT const unsigned char CredentialValidationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CredentialValidation/PublicHeader.h>
+
+
